06d403ac12223da0a9552db2baed9d49af627cd8
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2024-10-15 16:08:26 -0400

    Switching out namedtuble for class so that I can more easily mutate the state of other projects

--- app/script.py
+++ app/script.py
@@ -1,74 +1,77 @@
 
-from collections import namedtuple
 from datetime import date
 import app.constants as constants
 
-Dates = namedtuple('Dates', ["name", "start_date", "end_date", "is_high_cost"])
 
+class Project:
+    def __init__(self, name, start_date, end_date, is_high_cost):
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.is_high_cost = is_high_cost
+        self.travel_days = 2
+        self.full_days = (self.end_date.day - self.start_date.day - 1) or 0
 
-def parse_date(date_string):
-    month, day, year = date_string.strip().split("/")
-    year = ("20" + year).split(" ")[0]
-    return date(int(year), int(month), int(day))
+    @staticmethod
+    def parse_date(date_string):
+        month, day, year = date_string.strip().split("/")
+        year = ("20" + year).split(" ")[0]
+        return date(int(year), int(month), int(day))
 
+    @classmethod
+    def from_string(cls, string):
+        name, cost_string, start_date_str, end_date_str = string.split(":")
+        end_date = cls.parse_date(end_date_str)
+        start_date = cls.parse_date(start_date_str)
+        is_high_cost = "High" in cost_string
+        return cls(name, start_date, end_date, is_high_cost)
+  
+    def remove_travel_day(self):
+        self.travel_days -= 1
+        self.full_days += 1
 
-def parse_input(string):
-    name, cost_string, start_date_str, end_date_str = string.split(":")
-    end_date = parse_date(end_date_str)
-    start_date = parse_date(start_date_str)
-    is_high_cost = "High" in cost_string
-    return Dates(name, start_date, end_date, is_high_cost)
+    def calculate_project_cost(self):
+        if self.is_high_cost:
+            travel_day_cost = constants.TRAVEL_DAY_HIGH
+            full_day_cost = constants.FULL_DAY_HIGH
+        else:
+            travel_day_cost = constants.TRAVEL_DAY_LOW
+            full_day_cost = constants.FULL_DAY_LOW
+        return (self.full_days * full_day_cost) + (travel_day_cost * self.travel_days)
 
-
-def calculate_project_cost(project, max_travel_days: int, max_full_days: int):
-    high = project.is_high_cost
-    if high:
-        travel_day_cost = constants.TRAVEL_DAY_HIGH
-        full_day_cost = constants.FULL_DAY_HIGH
-    else:
-        travel_day_cost = constants.TRAVEL_DAY_LOW
-        full_day_cost = constants.FULL_DAY_LOW
-    return ((max_full_days * full_day_cost) + (travel_day_cost * max_travel_days))
-
-
-def base_case_full_days(project):
-    full_days = project.end_date.day - project.start_date.day
-    if full_days > 0:
-        full_days -= 1
-    return full_days
-
-
-def project_overlap(current_project, next_project):
-    return next_project.start_date.day - current_project.end_date.day
+    def project_overlap(self, next_project):
+        return next_project.start_date.day - self.end_date.day
 
 
 def calculate_reimbursement(list_of_strings):
     # Handles empty project sets
     if not list_of_strings:
         return
-    current_project = parse_input(list_of_strings[0])
+    current_project = Project.from_string(list_of_strings[0])
     # Single project is a special case
     if len(list_of_strings) == 1:
         return [
             {
-                current_project.name: calculate_project_cost(current_project, 2, base_case_full_days(current_project))}
+                current_project.name: current_project.calculate_project_cost()}
           ]
     results = []
     next_project_index = 1
     next_project = list_of_strings[next_project_index]
     while next_project:
-        max_travel_days = 2
-        max_full_days = base_case_full_days(current_project)
-        next_project = parse_input(next_project)
+        next_project = Project.from_string(next_project)
+        project_overlap = current_project.project_overlap(next_project)
         if current_project.end_date.day + 1 == next_project.start_date.day:
-              max_travel_days -= 1
-              max_full_days += 1
-                
-        cost = calculate_project_cost(
-            current_project,
-            max_full_days=max_full_days,
-            max_travel_days=max_travel_days
-            )
+            current_project.remove_travel_day()
+            next_project.remove_travel_day()
+        elif current_project.end_date.day >= next_project.start_date.day:
+            current_project.remove_travel_day()
+            next_project.remove_travel_day()
+            project_overlap = current_project.project_overlap(next_project)
+            if next_project.is_high_cost and not current_project.is_high_cost:
+                next_project.full_days -= project_overlap
+            else:
+                current_project.full_days -= project_overlap
+        cost = current_project.calculate_project_cost()
         results.append({current_project.name: cost})
         current_project = next_project
         next_project_index += 1
@@ -78,7 +81,7 @@ def calculate_reimbursement(list_of_strings):
         except IndexError:
             # current project is now next and final project
             # TODO Handle second to last project has higher cost and overlaps
-            cost = calculate_project_cost(current_project, 1, base_case_full_days(current_project))
+            cost = current_project.calculate_project_cost()
             results.append({current_project.name: cost})
             break
     return results
