3d335188527e0cd7370864b215b804864804ce4e
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2024-10-15 10:11:55 -0400

    Handle base case

--- tests.py
+++ tests.py
@@ -1,32 +1,29 @@
-from .. import constants
+import constants
+from script import calculate_reimbursement
 
 """
 We can assume for now that all input will be in this consistent format
 and that projects in each set will be chronological
 """
 
-set_1 =
-  ["Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/3/15"]
+set_1 = ["Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/3/15"]
 
-set_2 =
-  [
+set_2 = [
     "Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/1/15",
   "Project 2: High Cost City Start Date: 9/2/15 End Date: 9/6/15",
   "Project 3: Low Cost City Start Date: 9/6/15 End Date: 9/8/15"
   ]
 
-set_3 = 
-[
+set_3 = [
   "Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/3/15",
   "Project 2: High Cost City Start Date: 9/5/15 End Date: 9/7/15",
   "Project 3: High Cost City Start Date: 9/8/15 End Date: 9/8/15",
 ]
-set_4 = 
-[
+set_4 =[
   "Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/1/15",
   "Project 2: Low Cost City Start Date: 9/1/15 End Date: 9/1/15",
   "Project 3: High Cost City Start Date: 9/2/15 End Date: 9/2/15",
-  "Project 4: High Cost City Start Date: 9/2/15 End Date: 9/3/15
+  "Project 4: High Cost City Start Date: 9/2/15 End Date: 9/3/15"
 ]
 
-assert calculate_reimbursement(set_1) == [{"Project 1": }]
+assert calculate_reimbursement(set_1) == [{"Project 1": (constants.TRAVEL_DAY_LOW *2) + constants.FULL_DAY_LOW}]
