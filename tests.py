d5491ea5df0b3cc2ee69ad186ef4ba6336f7795c
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2024-10-15 10:29:37 -0400

    Calculate expected results for remaining examples

--- tests.py
+++ tests.py
@@ -10,8 +10,8 @@ set_1 = ["Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/3/15"]
 
 set_2 = [
     "Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/1/15",
-  "Project 2: High Cost City Start Date: 9/2/15 End Date: 9/6/15",
-  "Project 3: Low Cost City Start Date: 9/6/15 End Date: 9/8/15"
+    "Project 2: High Cost City Start Date: 9/2/15 End Date: 9/6/15",
+    "Project 3: Low Cost City Start Date: 9/6/15 End Date: 9/8/15"
   ]
 
 set_3 = [
@@ -27,3 +27,22 @@ set_4 =[
 ]
 
 assert calculate_reimbursement(set_1) == [{"Project 1": (constants.TRAVEL_DAY_LOW *2) + constants.FULL_DAY_LOW}]
+
+assert calculate_reimbursement(set_2) == [
+  {"Project 1": constants.TRAVEL_DAY_LOW},
+  {"Project 2": constants.FULL_DAY_HIGH*5}, 
+  {"Project 3": constants.TRAVEL_DAY_LOW + constants.FULL_DAY_LOW} # Don't count the first full day since it was already reimbursed in project 2
+]
+
+assert calculate_reimbursement(set_3) == [
+  {"Project 1": constants.TRAVEL_DAY_LOW*2 + constants.FULL_DAY_LOW},
+  {"Project 2": constants.TRAVEL_DAY_HIGH + (constants.FULL_DAY_HIGH*2)},
+  {"Project 3": constants.TRAVEL_DAY_HIGH}
+]
+
+assert calculate_reimbursement(set_4) == [
+  {"Project 1": constants.TRAVEL_DAY_LOW},
+  {"Project 2": 0.0},
+  {"Project 3": constants.FULL_DAY_HIGH},
+  {"Project 4": constants.TRAVEL_DAY_HIGH}
+]
