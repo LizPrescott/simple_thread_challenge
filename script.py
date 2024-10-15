6bcc75027a9fb216e69ce9b6cbee4fa25f9c196e
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2024-10-15 11:08:09 -0400

    Formatting fixes

--- script.py
+++ script.py
@@ -5,41 +5,41 @@ import constants
 
 Dates = namedtuple('Dates', ["name", "start_date", "end_date", "is_high_cost"])
 
+
 def parse_date(date_string):
-  month, day, year = date_string.strip().split("/")
-  year = ("20" + year).split(" ")[0]
-  return date(int(year), int(month), int(day))
+    month, day, year = date_string.strip().split("/")
+    year = ("20" + year).split(" ")[0]
+    return date(int(year), int(month), int(day))
+
 
 def parse_input(string):
-  name, cost_string, start_date_str, end_date_str = string.split(":")
-  end_date = parse_date(end_date_str)
-  start_date = parse_date(start_date_str)
-  is_high_cost = "High" in cost_string
-  return Dates(name, start_date, end_date, is_high_cost)
+    name, cost_string, start_date_str, end_date_str = string.split(":")
+    end_date = parse_date(end_date_str)
+    start_date = parse_date(start_date_str)
+    is_high_cost = "High" in cost_string
+    return Dates(name, start_date, end_date, is_high_cost)
+
 
 def calculate_project_cost(project):
     high = project.is_high_cost
     if high:
-      travel_day_cost = constants.TRAVEL_DAY_HIGH
-      full_day_cost = constants.FULL_DAY_HIGH
+        travel_day_cost = constants.TRAVEL_DAY_HIGH
+        full_day_cost = constants.FULL_DAY_HIGH
     else:
-      travel_day_cost = constants.TRAVEL_DAY_LOW
-      full_day_cost = constants.FULL_DAY_LOW
-    #TODO: Logic for contiguous projects in a set
+        travel_day_cost = constants.TRAVEL_DAY_LOW
+        full_day_cost = constants.FULL_DAY_LOW
+    # TODO: Logic for contiguous projects in a set
     #  Minus 1 when the last day is a travel day, subtract that day
     full_days = project.end_date.day - project.start_date.day - 1
     return ((full_days * full_day_cost) + (travel_day_cost*2))
 
 
 def calculate_reimbursement(list_of_strings):
-  # Handles empty project sets
-  if not list_of_strings:
-    return
-  current_project = parse_input(list_of_strings[0])
-  cost = calculate_project_cost(current_project)
-  #   if project.end_date == next.end_date:
-  #     total_cost += full_day_cost
-  return [{current_project.name: cost}]
-    
-
-
+    # Handles empty project sets
+    if not list_of_strings:
+        return
+    current_project = parse_input(list_of_strings[0])
+    cost = calculate_project_cost(current_project)
+    #   if project.end_date == next.end_date:
+    #     total_cost += full_day_cost
+    return [{current_project.name: cost}]
--- tests.py
+++ tests.py
@@ -19,30 +19,44 @@ set_3 = [
   "Project 2: High Cost City Start Date: 9/5/15 End Date: 9/7/15",
   "Project 3: High Cost City Start Date: 9/8/15 End Date: 9/8/15",
 ]
-set_4 =[
+set_4 = [
   "Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/1/15",
   "Project 2: Low Cost City Start Date: 9/1/15 End Date: 9/1/15",
   "Project 3: High Cost City Start Date: 9/2/15 End Date: 9/2/15",
   "Project 4: High Cost City Start Date: 9/2/15 End Date: 9/3/15"
 ]
 
-assert calculate_reimbursement(set_1) == [{"Project 1": (constants.TRAVEL_DAY_LOW *2) + constants.FULL_DAY_LOW}]
 
-assert calculate_reimbursement(set_2) == [
-  {"Project 1": constants.TRAVEL_DAY_LOW},
-  {"Project 2": constants.FULL_DAY_HIGH*5}, 
-  {"Project 3": constants.TRAVEL_DAY_LOW + constants.FULL_DAY_LOW} # Don't count the first full day since it was already reimbursed in project 2
-]
+def test_base_case():
+    assert calculate_reimbursement(set_1) == [
+        {"Project 1": (constants.TRAVEL_DAY_LOW * 2) + constants.FULL_DAY_LOW}
+    ]
 
-assert calculate_reimbursement(set_3) == [
-  {"Project 1": constants.TRAVEL_DAY_LOW*2 + constants.FULL_DAY_LOW},
-  {"Project 2": constants.TRAVEL_DAY_HIGH + (constants.FULL_DAY_HIGH*2)},
-  {"Project 3": constants.TRAVEL_DAY_HIGH}
-]
 
-assert calculate_reimbursement(set_4) == [
-  {"Project 1": constants.TRAVEL_DAY_LOW},
-  {"Project 2": 0.0},
-  {"Project 3": constants.FULL_DAY_HIGH},
-  {"Project 4": constants.TRAVEL_DAY_HIGH}
-]
+def test_high_to_low_overlap():
+    assert calculate_reimbursement(set_2) == [
+      {"Project 1": constants.TRAVEL_DAY_LOW},
+      # Assume higher cost full day should be reimbursed
+      {"Project 2": constants.FULL_DAY_HIGH*5},
+      # Don't count the first full day since, already reimbursed in project 2
+      {"Project 3": constants.TRAVEL_DAY_LOW + constants.FULL_DAY_LOW}
+    ]
+
+
+def test_multiple_projects_no_overlaps():
+    assert calculate_reimbursement(set_3) == [
+      {"Project 1": constants.TRAVEL_DAY_LOW*2 + constants.FULL_DAY_LOW},
+      {"Project 2": constants.TRAVEL_DAY_HIGH + (constants.FULL_DAY_HIGH*2)},
+      {"Project 3": constants.TRAVEL_DAY_HIGH}
+    ]
+
+
+def test_travel_day_perfect_overlap():
+    assert calculate_reimbursement(set_4) == [
+      # Assum when one of the overlap days is a travel day, both are
+      {"Project 1": constants.TRAVEL_DAY_LOW},
+      {"Project 2": 0.0},
+      {"Project 3": constants.FULL_DAY_HIGH},
+      # Do not count the full day, which was reimbursed in Project 3
+      {"Project 4": constants.TRAVEL_DAY_HIGH}
+    ]
