ba6f3b628206dd1ab4fd035591d8d736e61a9eac
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2024-10-22 11:33:03 -0400

    All tests pass with original function simplified

--- app/script.py
+++ app/script.py
@@ -1,6 +1,23 @@
 
 from datetime import date
-import app.constants as constants
+from app.constants import (
+    FULL_DAY_HIGH,
+    FULL_DAY_LOW,
+    TRAVEL_DAY_HIGH,
+    TRAVEL_DAY_LOW
+)
+
+
+class Reimbursement:
+    """
+    A generator class, for looping over all projects in a set
+    """
+    def __init__(self, list_of_strings):
+        self.projects = [Project.from_string(x) for x in list_of_strings]
+
+    def __iter__(self):
+        for project in self.projects:
+            yield project
 
 
 class Project:
@@ -12,6 +29,7 @@ class Project:
         self.travel_days = 2
         # Allow full days to get below zero to account for diffs in travel days
         self.full_days = (self.end_date - self.start_date).days - 1
+        self.duration = max(((self.end_date - self.start_date).days - 1),0)
 
     @staticmethod
     def parse_date(date_string):
@@ -33,11 +51,11 @@ class Project:
 
     def calculate_project_cost(self):
         if self.is_high_cost:
-            travel_day_cost = constants.TRAVEL_DAY_HIGH
-            full_day_cost = constants.FULL_DAY_HIGH
+            travel_day_cost = TRAVEL_DAY_HIGH
+            full_day_cost = FULL_DAY_HIGH
         else:
-            travel_day_cost = constants.TRAVEL_DAY_LOW
-            full_day_cost = constants.FULL_DAY_LOW
+            travel_day_cost = TRAVEL_DAY_LOW
+            full_day_cost = FULL_DAY_LOW
         return max((self.full_days * full_day_cost) + (travel_day_cost * self.travel_days), 0)
 
     def project_overlap(self, next_project):
@@ -48,42 +66,79 @@ def calculate_reimbursement(list_of_strings):
     # Handles empty project sets
     if not list_of_strings:
         return
-    current_project = Project.from_string(list_of_strings[0])
+    starting_iter = iter(Reimbursement(list_of_strings))
+    current_project = next(starting_iter)
     # Single project is a special case
     if len(list_of_strings) == 1:
         return current_project.calculate_project_cost()
     total = 0.0
-    next_project_index = 1
-    next_project = list_of_strings[next_project_index]
-    while next_project:
-        next_project = Project.from_string(next_project)
+    for next_project in starting_iter:
         project_overlap = current_project.project_overlap(next_project)
+        if project_overlap:
+            pass
         if (next_project.start_date - current_project.end_date).days == 1:
             current_project.remove_travel_day()
             next_project.remove_travel_day()
         elif current_project.end_date >= next_project.start_date:
             current_project.remove_travel_day()
             next_project.remove_travel_day()
-            project_overlap = current_project.project_overlap(next_project)
-            if current_project.is_high_cost:
+            if not next_project.is_high_cost:
                 next_project.full_days -= project_overlap
             else:
                 current_project.full_days -= project_overlap
         total += current_project.calculate_project_cost()
         current_project = next_project
-        next_project_index += 1
-        # catch the index error on last project. Will not have a next project
-        try:
-            next_project = list_of_strings[next_project_index]
-        except IndexError:
-            total += current_project.calculate_project_cost()
-            break
+    # Final Project costs
+    total += current_project.calculate_project_cost()
     return total
 
-
+# Experimental refactor that I'm not read to let go yet
+# 
 # def calculate_total(list_of_strings):
+#     pass
 #     # Handles empty project sets
 #     if not list_of_strings:
 #         return 0.0
-#     for string in list_of_strings:
-#         travel_cost
+#     proj_iter = iter(Reimbursement(list_of_strings))
+#     proj_a = next(proj_iter)
+#     total = 0.0
+#     high_cost = proj_a.is_high_cost
+#     travel_cost = TRAVEL_DAY_HIGH if high_cost else TRAVEL_DAY_LOW
+#     full_cost = FULL_DAY_HIGH if high_cost else FULL_DAY_LOW
+#     for proj_b in proj_iter:
+#         pass
+#         total += travel_cost * 2
+#         total += full_cost * current_project.duration
+#         proj_a = proj_b
+#     total += travel_cost
+#     return total
+    # Find the first travel day
+    # Many projects can start on the same day and be a single day
+    # while current_project.start_date == current_project.end_date == next_project.start_date:
+    #     current_project = next(proj_iter)
+    #     next_project = next(next_proj_iter)
+    #     high_cost = high_cost or current_project.is_high_cost
+    # travel_cost = TRAVEL_DAY_HIGH if high_cost else TRAVEL_DAY_LOW
+    # full_cost = FULL_DAY_HIGH if high_cost else FULL_DAY_LOW
+    # # Opening travel day
+    # total += travel_cost
+    # total += full_cost * max(((current_project.end_date - current_project.start_date).days - 1),0)
+    # # TODO Determine status of the final day of the project
+    # for n in range(next_project_index, len(list_of_strings)):
+    #     if (next_project.start_date - current_project.end_date).days > 1:
+    #         # End of previous 
+    #         total += travel_cost
+    #     current_project = next_project
+    #     travel_cost = TRAVEL_DAY_HIGH if current_project.is_high_cost else TRAVEL_DAY_LOW
+    #     full_cost = FULL_DAY_HIGH if current_project.is_high_cost else FULL_DAY_LOW
+    #     total += (full_cost * (current_project.end_date - current_project.start_date).days)
+    #     next_project_index += 1
+    #     next_project = Project.from_string(list_of_strings[n])
+    # final_project = Project.from_string(list_of_strings[-1])
+    # total += travel_cost
+    # if (current_project.end_date - final_project.start_date).days > 1:
+    #     # Closing travel date
+    #     total += travel_cost
+    # total += full_cost * final_project.duration
+    # return total    
+            
--- app/tests.py
+++ app/tests.py
@@ -1,8 +1,10 @@
 from app.constants import (
-  FULL_DAY_HIGH, FULL_DAY_LOW, TRAVEL_DAY_HIGH, TRAVEL_DAY_LOW
+  FULL_DAY_HIGH,
+  FULL_DAY_LOW,
+  TRAVEL_DAY_HIGH,
+  TRAVEL_DAY_LOW
 )
 from app.script import calculate_reimbursement
-
 
 """
 We can assume for now that all input will be in this consistent format
@@ -14,6 +16,7 @@ def test_base_case():
     input = ["Project 1: Low Cost City Start Date: 9/1/15 End Date: 9/3/15"]
     project_1_total = (TRAVEL_DAY_LOW * 2) + FULL_DAY_LOW
     assert calculate_reimbursement(input) == project_1_total
+    # assert calculate_total(input) == project_1_total
 
 
 def test_high_to_low_overlap():
@@ -28,6 +31,9 @@ def test_high_to_low_overlap():
     assert calculate_reimbursement(input) == sum([
          project_1_total, project_2_total, project_3_total
          ])
+    # assert calculate_total(input) == sum([
+    #      project_1_total, project_2_total, project_3_total
+    #      ])
 
 
 def test_contiguous_projects():
@@ -36,12 +42,15 @@ def test_contiguous_projects():
       "Project 2: High Cost City Start Date: 9/5/15 End Date: 9/7/15",
       "Project 3: High Cost City Start Date: 9/8/15 End Date: 9/8/15",
     ]
-    project_1_total = ((TRAVEL_DAY_LOW*2) + FULL_DAY_LOW)  # 195
-    project_2_total = (TRAVEL_DAY_HIGH + (FULL_DAY_HIGH*2))  # 195
+    project_1_total = ((TRAVEL_DAY_LOW*2) + FULL_DAY_LOW)
+    project_2_total = (TRAVEL_DAY_HIGH + (FULL_DAY_HIGH*2))
     project_3_total = TRAVEL_DAY_HIGH
-    assert calculate_reimbursement(input) == sum([
+    assert calculate_reimbursement(input) == sum([ # sum([165.0, 225.0, 55.0])
       project_1_total, project_2_total, project_3_total
     ])
+    # assert calculate_total(input) == sum([
+    #   project_1_total, project_2_total, project_3_total
+    # ])
 
 
 def test_travel_day_perfect_overlap():
@@ -51,10 +60,14 @@ def test_travel_day_perfect_overlap():
       "Project 3: High Cost City Start Date: 9/2/15 End Date: 9/2/15",
       "Project 4: High Cost City Start Date: 9/2/15 End Date: 9/3/15"
     ]
-    project_1_total = 0.0,
-    project_2_total = TRAVEL_DAY_HIGH,
+    project_1_total = 0.0
+    project_2_total = TRAVEL_DAY_LOW
     project_3_total = FULL_DAY_HIGH
     project_4_total = TRAVEL_DAY_HIGH
+    # assert calculate_total(input) == sum([
+    #   project_1_total, project_2_total, project_3_total, project_4_total
+    # ])
     assert calculate_reimbursement(input) == sum([
       project_1_total, project_2_total, project_3_total, project_4_total
     ])
+    
