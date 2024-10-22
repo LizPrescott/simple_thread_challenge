983908f87dd1eeeb244f61fbf609cbb97e1cfe4b
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2024-10-22 17:20:42 -0400

    Refactor out Project.handle_overlap

--- app/script.py
+++ app/script.py
@@ -59,7 +59,19 @@ class Project:
         return max((self.full_days * full_day_cost) + (travel_day_cost * self.travel_days), 0)
 
     def project_overlap(self, next_project):
-        return next_project.start_date.day - self.end_date.day + 1
+        return (next_project.start_date - self.end_date).days + 1
+
+    def handle_overlap(self, next_project, project_overlap):
+        if (next_project.start_date - self.end_date).days == 1:
+            self.remove_travel_day()
+            next_project.remove_travel_day()
+        elif self.end_date >= next_project.start_date:
+            self.remove_travel_day()
+            next_project.remove_travel_day()
+            if not next_project.is_high_cost:
+                next_project.full_days -= project_overlap
+            else:
+                self.full_days -= project_overlap
 
 
 def calculate_reimbursement(list_of_strings):
@@ -71,74 +83,17 @@ def calculate_reimbursement(list_of_strings):
     # Single project is a special case
     if len(list_of_strings) == 1:
         return current_project.calculate_project_cost()
-    total = 0.0
+    total = 0
     for next_project in starting_iter:
         project_overlap = current_project.project_overlap(next_project)
-        if project_overlap:
-            pass
-        if (next_project.start_date - current_project.end_date).days == 1:
-            current_project.remove_travel_day()
-            next_project.remove_travel_day()
-        elif current_project.end_date >= next_project.start_date:
-            current_project.remove_travel_day()
-            next_project.remove_travel_day()
-            if not next_project.is_high_cost:
-                next_project.full_days -= project_overlap
-            else:
-                current_project.full_days -= project_overlap
+        if project_overlap > 0:
+            current_project.handle_overlap(next_project, project_overlap)
         total += current_project.calculate_project_cost()
         current_project = next_project
     # Final Project costs
     total += current_project.calculate_project_cost()
     return total
 
-# Experimental refactor that I'm not read to let go yet
-# 
-# def calculate_total(list_of_strings):
-#     pass
-#     # Handles empty project sets
-#     if not list_of_strings:
-#         return 0.0
-#     proj_iter = iter(Reimbursement(list_of_strings))
-#     proj_a = next(proj_iter)
-#     total = 0.0
-#     high_cost = proj_a.is_high_cost
-#     travel_cost = TRAVEL_DAY_HIGH if high_cost else TRAVEL_DAY_LOW
-#     full_cost = FULL_DAY_HIGH if high_cost else FULL_DAY_LOW
-#     for proj_b in proj_iter:
-#         pass
-#         total += travel_cost * 2
-#         total += full_cost * current_project.duration
-#         proj_a = proj_b
-#     total += travel_cost
-#     return total
-    # Find the first travel day
-    # Many projects can start on the same day and be a single day
-    # while current_project.start_date == current_project.end_date == next_project.start_date:
-    #     current_project = next(proj_iter)
-    #     next_project = next(next_proj_iter)
-    #     high_cost = high_cost or current_project.is_high_cost
-    # travel_cost = TRAVEL_DAY_HIGH if high_cost else TRAVEL_DAY_LOW
-    # full_cost = FULL_DAY_HIGH if high_cost else FULL_DAY_LOW
-    # # Opening travel day
-    # total += travel_cost
-    # total += full_cost * max(((current_project.end_date - current_project.start_date).days - 1),0)
-    # # TODO Determine status of the final day of the project
-    # for n in range(next_project_index, len(list_of_strings)):
-    #     if (next_project.start_date - current_project.end_date).days > 1:
-    #         # End of previous 
-    #         total += travel_cost
-    #     current_project = next_project
-    #     travel_cost = TRAVEL_DAY_HIGH if current_project.is_high_cost else TRAVEL_DAY_LOW
-    #     full_cost = FULL_DAY_HIGH if current_project.is_high_cost else FULL_DAY_LOW
-    #     total += (full_cost * (current_project.end_date - current_project.start_date).days)
-    #     next_project_index += 1
-    #     next_project = Project.from_string(list_of_strings[n])
-    # final_project = Project.from_string(list_of_strings[-1])
-    # total += travel_cost
-    # if (current_project.end_date - final_project.start_date).days > 1:
-    #     # Closing travel date
-    #     total += travel_cost
-    # total += full_cost * final_project.duration
-    # return total    
-            
+
+
+                    
--- app/tests.py
+++ app/tests.py
@@ -26,8 +26,8 @@ def test_high_to_low_overlap():
       "Project 3: Low Cost City Start Date: 9/6/15 End Date: 9/8/15"
     ]
     project_1_total = TRAVEL_DAY_LOW
-    project_2_total = FULL_DAY_HIGH*5
-    project_3_total = TRAVEL_DAY_LOW + FULL_DAY_LOW
+    project_2_total = FULL_DAY_HIGH*5 # 9/2 + 9/3 + 9/4 + 9/5 + 9/6
+    project_3_total = TRAVEL_DAY_LOW + FULL_DAY_LOW # 9/8 + 9/7
     assert calculate_reimbursement(input) == sum([
          project_1_total, project_2_total, project_3_total
          ])
@@ -45,7 +45,7 @@ def test_contiguous_projects():
     project_1_total = ((TRAVEL_DAY_LOW*2) + FULL_DAY_LOW)
     project_2_total = (TRAVEL_DAY_HIGH + (FULL_DAY_HIGH*2))
     project_3_total = TRAVEL_DAY_HIGH
-    assert calculate_reimbursement(input) == sum([ # sum([165.0, 225.0, 55.0])
+    assert calculate_reimbursement(input) == sum([ # sum([165, 225, 55])
       project_1_total, project_2_total, project_3_total
     ])
     # assert calculate_total(input) == sum([
@@ -60,7 +60,7 @@ def test_travel_day_perfect_overlap():
       "Project 3: High Cost City Start Date: 9/2/15 End Date: 9/2/15",
       "Project 4: High Cost City Start Date: 9/2/15 End Date: 9/3/15"
     ]
-    project_1_total = 0.0
+    project_1_total = 0
     project_2_total = TRAVEL_DAY_LOW
     project_3_total = FULL_DAY_HIGH
     project_4_total = TRAVEL_DAY_HIGH
